c392acdc3d7a6d69e64d45f1b741aaef10e8d3e7
Jeroen Noels <jeroen.noels@telenet.be>
2020-12-06 15:46:47 +0100

    first drafts

--- clean.sh
+++ clean.sh
@@ -2,4 +2,4 @@
 find ../essence-ad/ -name "*.o"  -print0 | xargs -r -0 /bin/rm
 find ../essence-ad/ -name "*.hi" -print0 | xargs -r -0 /bin/rm
 find ../essence-ad/ -name "*~"   -print0 | xargs -r -0 /bin/rm
-/bin/rm ../essence-ad/test/Main
+/bin/rm -f ../essence-ad/test/Main
